cd2c2e7bc375067be9d4ed5578960a8c71fdca87
lakegreene <lakegreene@yahoo.com>
2020-09-13 15:19:05 -0400

    Create feature to post comments on videos

--- src/app/comment.service.ts
+++ src/app/comment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Comment } from './comment';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -12,9 +12,17 @@ export class CommentService {
   constructor(private httpClient: HttpClient) { }
 
   private commentUrl = 'https://channelcashmoney.herokuapp.com/api/comments';
+  httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
 
   getComments(video_id: number): Observable<Comment[]>{
-      console.log(`Fetching Comments for Video Number ${video_id}`)
-      return this.httpClient.get<Comment[]>(this.commentUrl + `/id=${video_id}`)
-    }
+    console.log(`Fetching Comments for Video Number ${video_id}`)
+    return this.httpClient.get<Comment[]>(this.commentUrl + `/id=${video_id}`)
+  }
+
+  postComment(comment: Comment, video_id: number): Observable<Comment> {
+    return this.httpClient.post<Comment>(this.commentUrl + `/id=${video_id}`, comment, this.httpOptions);
+  }
+
 }
--- src/app/video-comments/video-comments.component.ts
+++ src/app/video-comments/video-comments.component.ts
@@ -20,6 +20,11 @@ export class VideoCommentsComponent implements OnChanges {
   }
 
   getVideoComments(){
-      this.commentService.getComments(this.videoId).subscribe(comments => this.comments = comments);
+    this.commentService.getComments(this.videoId).subscribe(comments => this.comments = comments);
+  }
+
+  addVideoComment(text: string): void {
+    let comment:  Comment = { comment_id: null, comment_text: text.trim(), comment_timestamp: null, video_id: null} ;
+    this.commentService.postComment(comment, this.videoId).subscribe(comment => this.comments.push(comment));
   }
 }
